571ee6af92d36e5b7cd8dcf49c7f3df8b5806f04
zhou <879863325@qq.com>
2018-04-19 22:05:32 +0800

    完善音乐播放

--- readermovie/data/data.js
+++ readermovie/data/data.js
@@ -13,7 +13,7 @@ var local_database = [
     detail: "菊黄蟹正肥，品尝秋之味。徐志摩把“看初花的荻芦”和“到楼外楼吃蟹”并列为秋天来杭州不能错过的风雅之事；用林妹妹的话讲是“螯封嫩玉双双满，壳凸红脂块块香”；在《世说新语》里，晋毕卓更是感叹“右手持酒杯，左手持蟹螯，拍浮酒船中，便足了一生矣。”漫漫人生长路，美食与爱岂可辜负？于是作为一个吃货，突然也很想回味一下属于我的味蕾记忆。记忆中的秋蟹，是家人的味道，弥漫着浓浓的亲情。\n\n是谁来自山川湖海，却囿于昼夜，厨房与爱？ 是母亲，深思熟虑，聪明耐心。吃蟹前，总会拿出几件工具，煞有介事而乐此不疲。告诉我们螃蟹至寒，需要佐以姜茶以祛寒，在配备的米醋小碟里，亦添入姜丝与紫苏，前者驱寒后者增香。泡好菊花茶，岁月静好，我们静等。",
     postId: 0,
     music: {
-      url: "http://ws.stream.qqmusic.qq.com/C100003507bR0gDKBm.m4a?fromtag=38",
+      url: 'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E061FF02C31F716658E5C81F5594D561F2E88B854E81CAAB7806D5E4F103E55D33C16F3FAC506D1AB172DE8600B37E43FAD&fromtag=46',
       title: "夜夜夜夜-齐秦",
       coverImg: "http://y.gtimg.cn/music/photo_new/T002R150x150M000001TEc6V0kjpVC.jpg?max_age=2592000"
     }
@@ -32,7 +32,7 @@ var local_database = [
     avatar: "/image/avatar/1.png",
     postId: 1,
     music: {
-      url: "http://ws.stream.qqmusic.qq.com/C100003GdCmG4NkEOR.m4a?fromtag=38",
+      url: 'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E061FF02C31F716658E5C81F5594D561F2E88B854E81CAAB7806D5E4F103E55D33C16F3FAC506D1AB172DE8600B37E43FAD&fromtag=46',
       title: "鬼迷心窍-李宗盛",
       coverImg: "http://y.gtimg.cn/music/photo_new/T002R150x150M000002xOmp62kqSic.jpg?max_age=2592000"
     }
@@ -52,7 +52,7 @@ var local_database = [
     avatar: "/image/avatar/3.png",
     postId: 2,
     music: {
-      url: "http://ws.stream.qqmusic.qq.com/C100004HLusI2lLjZy.m4a?fromtag=38",
+      url: 'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E061FF02C31F716658E5C81F5594D561F2E88B854E81CAAB7806D5E4F103E55D33C16F3FAC506D1AB172DE8600B37E43FAD&fromtag=46',
       title: "女儿情-万晓利",
       coverImg: "http://y.gtimg.cn/music/photo_new/T002R150x150M000004Wv5BO30pPc0.jpg?max_age=2592000"
     }
@@ -71,7 +71,7 @@ var local_database = [
     avatar: "/image/avatar/5.png",
     postId: 3,
     music: {
-      url: "http://ws.stream.qqmusic.qq.com/C100002mWVx72p8Ugp.m4a?fromtag=38",
+      url: 'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E061FF02C31F716658E5C81F5594D561F2E88B854E81CAAB7806D5E4F103E55D33C16F3FAC506D1AB172DE8600B37E43FAD&fromtag=46',
       title: "恋恋风尘-老狼",
       coverImg: "http://y.gtimg.cn/music/photo_new/T002R150x150M000001VaXQX1Z1Imq.jpg?max_age=2592000",
     }
@@ -90,7 +90,7 @@ var local_database = [
     avatar: "../../../image/avatar/3.png",
     postId: 4,
     music: {
-      url: "http://ws.stream.qqmusic.qq.com/C100000Zn0vS4fKKo8.m4a?fromtag=38",
+      url: 'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E061FF02C31F716658E5C81F5594D561F2E88B854E81CAAB7806D5E4F103E55D33C16F3FAC506D1AB172DE8600B37E43FAD&fromtag=46',
       title: "沉默是金-张国荣",
       coverImg: "http://y.gtimg.cn/music/photo_new/T002R150x150M000003at0mJ2YrR2H.jpg?max_age=2592000"
     }
@@ -109,7 +109,7 @@ var local_database = [
     avatar: "../../../image/avatar/5.png",
     postId: 5,
     music: {
-      url: "http://ws.stream.qqmusic.qq.com/C100002I8eGJ28BI17.m4a?fromtag=38",
+      url: 'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E061FF02C31F716658E5C81F5594D561F2E88B854E81CAAB7806D5E4F103E55D33C16F3FAC506D1AB172DE8600B37E43FAD&fromtag=46',
       title: "朋友-谭咏麟",
       coverImg: "http://y.gtimg.cn/music/photo_new/T002R150x150M000004eGsCN3SUheO.jpg?max_age=2592000"
     }
--- readermovie/pages/posts/post-detail/post-detail.js
+++ readermovie/pages/posts/post-detail/post-detail.js
@@ -1,43 +1,79 @@
 var postsData = require('../../../data/data.js');
+var app = getApp();
 Page({
+  data: {
+    isPlay: false
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    var globalData = app.globalData;
+
     var id = options.id;
     var postData = postsData.postList[id];
     this.setData(postData);
     this.data.id = id;
 
 
+
     //设置是否收藏
     var storageList = wx.getStorageSync("storage");
-    if(storageList){
+    if (storageList) {
       var isStorage = storageList[id];
       this.setData({
-        collected : isStorage
+        collected: isStorage
       })
-    }else{
+    } else {
       var storage = {};
-       storage[id] =false;
-       wx.setStorageSync("storage", storage)
+      storage[id] = false;
+      wx.setStorageSync("storage", storage)
     }
+
+    if (globalData.g_isPlay && globalData.g_currentId == id) {
+      this.data.isPlay = true;
+      this.setData({
+        isPlayed: true
+      })
+    }
+
+    this.setMusicMonitor();
+  },
+
+  setMusicMonitor: function () {
+    var that = this;
+    wx.onBackgroundAudioPause(function () {
+      that.setData({
+        isPlayed: false,
+      })
+      app.globalData.g_isPlay = false;
+      app.globalData.g_currentId = null;
+    })
+
+    wx.onBackgroundAudioPlay(function () {
+      that.setData({
+        isPlayed: true,
+
+      })
+      app.globalData.g_isPlay = true;
+      app.globalData.g_currentId = that.data.id;
+    })
   },
 
   onCollectionTap: function (event) {
-   var storage =  wx.getStorageSync("storage");
-   var isStorage = storage[this.data.id];
+    var storage = wx.getStorageSync("storage");
+    var isStorage = storage[this.data.id];
     isStorage = !isStorage;
     storage[this.data.id] = isStorage;
     wx.setStorageSync("storage", storage)
 
     this.setData({
-      collected:isStorage
+      collected: isStorage
     })
 
     wx.showToast({
-      title: isStorage?"收藏成功":"取消成功",
-      icon:"none"
+      title: isStorage ? "收藏成功" : "取消成功",
+      icon: "none"
     })
 
     wx.showModal({
@@ -53,22 +89,43 @@ Page({
         '分享到QQ',
         '分享到微博'
       ],
-      itemColor:"#666",
+      itemColor: "#666",
 
-      success:function(res){
+      success: function (res) {
         console.log(res.tapIndex);
       }
     })
   },
 
-  onMusicTap:function(event){
-    console.log("play");
-    wx.playBackgroundAudio({
-      dataUrl: 'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E061FF02C31F716658E5C81F5594D561F2E88B854E81CAAB7806D5E4F103E55D33C16F3FAC506D1AB172DE8600B37E43FAD&fromtag=46',
-      title:'沉默是金-张国荣',
-      coverImgUrl: 'http://y.gtimg.cn/music/photo_new/T002R150x150M000003at0mJ2YrR2H.jpg?max_age=2592000'
-    })
-  }
+  onMusicTap: function (event) {
+    var isPlay = this.data.isPlay;
+    var postId = this.data.id;
+    if (isPlay) {
+      wx.stopBackgroundAudio();
+      this.setData({
+        isPlayed: false
+      });
+      this.data.isPlay = false;
+      app.globalData.g_isPlay = false;
+
+    } else {
+      console.log("play");
+      wx.playBackgroundAudio({
+        dataUrl: postsData.postList[postId].music.url,
+        // dataUrl: 'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E061FF02C31F716658E5C81F5594D561F2E88B854E81CAAB7806D5E4F103E55D33C16F3FAC506D1AB172DE8600B37E43FAD&fromtag=46',
+        title: postsData.postList[postId].music.title,
+        coverImgUrl: postsData.postList[postId].music.coverImg
+      })
+
+      this.setData({
+        isPlayed: true
+      });
+      this.data.isPlay = true;
+      app.globalData.g_isPlay = true;
+
+    }
+  },
+
 
 
 })
